26d0a1c021aab8eaae11475e5bd9da50cf404750
Lifo Fernandez <lifofernandez@gmail.com>
2016-06-09 19:27:22 -0300

    inital commit

---
+++ app.js
@@ -0,0 +1,43 @@
+var app = require('http').createServer(handler)
+var io = require('socket.io')(app);
+var fs = require('fs');
+
+app.listen(80);
+
+function handler (req, res) {
+  fs.readFile(__dirname + '/index.html',
+  function (err, data) {
+    if (err) {
+      res.writeHead(500);
+      return res.end('Error loading index.html');
+    }
+
+    res.writeHead(200);
+    res.end(data);
+  });
+}
+
+
+// Main Vars
+var tempo = 1000;
+
+
+setInterval(
+  function(){   
+    io.sockets.emit('click', { play: true });
+  }, 
+  tempo
+);
+
+
+
+
+io.on('connection', function (socket) {
+  socket.emit('news', { hello: 'world' });
+
+
+  socket.on('my other event', function (data) {
+     console.log(data);
+  });
+
+});
